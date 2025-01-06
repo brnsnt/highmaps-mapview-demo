1ca7717b6eba153c9371ad3040801f32ec0cbe5e
Bruno Senti <brnsnt@protonmail.ch>
2025-01-06 16:50:35 +0100

    initial commit - map view example

---
+++ src/components/HighmapsDemo.vue
@@ -0,0 +1,22 @@
+<script setup>
+import MapChart from "./MapChart.vue";
+import { ref } from "vue";
+
+const mapView = ref({ center: [0, 0], zoom: 2, chart: null });
+</script>
+
+<template>
+  <div>
+    Center: {{ mapView.center }}, Zoom: {{ mapView.zoom }}, Master:
+    {{ mapView.chart }}
+  </div>
+
+  <div style="display: grid; grid-template-columns: 1fr 1fr; padding: 1em">
+    <div>
+      <MapChart title="Chart 1" v-model:mapView="mapView" />
+    </div>
+    <div>
+      <MapChart title="Chart 2" v-model:mapView="mapView" />
+    </div>
+  </div>
+</template>
